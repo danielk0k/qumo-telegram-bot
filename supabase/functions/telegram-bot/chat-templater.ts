755b50fc0c7385a31082dbd3679ae884b09decca
danielk0k <70556073+danielk0k@users.noreply.github.com>
2024-03-19 21:42:20 +0800

    Improve system prompt

--- supabase/functions/telegram-bot/chat-templater.ts
+++ supabase/functions/telegram-bot/chat-templater.ts
@@ -2,12 +2,11 @@ const SYSTEM_TOKEN = "<|system|>";
 const USER_TOKEN = "<|user|>";
 const ASSISTANT_TOKEN = "<|assistant|>";
 const END_OF_STRING = "</s>";
-const DEFAULT_SYSTEM_PROMPT =
-  "You are a friendly chatbot assisting a researcher to interview a particpant and always responds with a relevant question to the previous reply.";
+const DEFAULT_SYSTEM_PROMPT = "As a friendly chatbot, you're assisting a researcher in conducting a research interview. Your role is to engage with the participant by responding with relevant questions based on their previous replies, ensuring a smooth and natural flow of conversation.";
 
 export function apply_chat_template(
   chat: { question: string; response: string }[],
-  research_purpose: string
+  research_purpose: string,
 ) {
   let output =
     `${SYSTEM_TOKEN} ${research_purpose}\n${DEFAULT_SYSTEM_PROMPT}${END_OF_STRING}\n${USER_TOKEN}\nStart asking me questions${END_OF_STRING}\n`;
